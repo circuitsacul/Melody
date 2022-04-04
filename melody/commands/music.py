7bf2edcecdfc56c28efb874062fc3a4b1932dff8
Circuit <circuitsacul@icloud.com>
2022-04-04 16:46:30 -0400

    Support for playlists, fix queue, /seek cmd

--- melody/commands/music.py
+++ melody/commands/music.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import datetime
+from inspect import isawaitable
 from typing import TYPE_CHECKING, cast
 
 import crescent
@@ -48,9 +49,14 @@ class PlaySong:
         await bot.join_vc(ctx.guild_id, vc_state.channel_id)
 
         source = await bot.play_url(ctx.guild_id, self.url)
-        await ctx.respond(
-            "Added to queue: " + song_infostr(await source.metadata())
-        )
+        if isinstance(source, list):
+            await ctx.respond(
+                "Added {} songs to queue.".format(len(source)), ephemeral=True
+            )
+        else:
+            await ctx.respond(
+                "Added to queue: " + song_infostr(await source.metadata())
+            )
 
 
 @plugin.include
@@ -153,6 +159,32 @@ class SkipTrack:
 
 @plugin.include
 @crescent.hook(guild_only)
+@crescent.hook(vc_match)
+@crescent.command(name="seek", description="Seek to a specific time.")
+class SeekTrack:
+    seconds = crescent.option(
+        int,
+        "The time to seek to.",
+        min_value=0,
+    )
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        bot = cast("Bot", ctx.app)
+        assert ctx.guild_id is not None
+
+        player = bot.players.get(ctx.guild_id)
+        if not player or not player.queue.track_handle:
+            raise MelodyErr("There's no song playing.")
+
+        if not player.queue.track_handle.is_seekable:
+            raise MelodyErr("This song is not seekable.")
+
+        player.queue.track_handle.seek_time(self.seconds)
+        await ctx.respond(f"Set position to {self.seconds}.")
+
+
+@plugin.include
+@crescent.hook(guild_only)
 @crescent.command(
     name="queue", description="Show the currently playing and upcoming songs."
 )
@@ -167,10 +199,20 @@ class ShowQueue:
 
         np = player.queue.track_handle
 
+        current_len = 0
         upcoming: list[str] = []
-        for track in player.queue:
-            meta = await track.metadata()
-            upcoming.append(song_infostr(meta))
+        for x, track in enumerate(player.queue):
+            meta = track.metadata
+            if hasattr(meta, "__call__"):
+                meta = meta()
+            if isawaitable(meta):
+                meta = await meta
+            infostr = f"{x}. {song_infostr(meta)}"
+            if (current_len + len(infostr)) + 2*len(upcoming) > 2000:
+                upcoming.append("And others...")
+                break
+            current_len += len(infostr)
+            upcoming.append(infostr)
 
         if np is None and not len(upcoming):
             raise MelodyErr("The queue is empty!")
@@ -189,5 +231,44 @@ class ShowQueue:
                     else ""
                 )
             ),
+        )
+        await ctx.respond(embed=embed)
+
+
+@plugin.include
+@crescent.hook(guild_only)
+@crescent.command(
+    name="nowplaying", description="Show the currently playing song."
+)
+class ShowNowPlaying:
+    async def callback(self, ctx: crescent.Context) -> None:
+        bot = cast("Bot", ctx.app)
+        assert ctx.guild_id is not None
+
+        player = bot.players.get(ctx.guild_id)
+        if player is None:
+            raise MelodyErr("There is no song playing!")
+
+        np = player.queue.track_handle
+
+        if np is None:
+            raise MelodyErr("There is no song playing!")
+
+        state = await np.get_info()
+        current = str(datetime.timedelta(seconds=int(state.position)))
+        total = str(datetime.timedelta(seconds=int(np.metadata.duration or 0)))
+
+        embed = hikari.Embed(
+            title=np.metadata.title,
+            color=CONFIG.theme,
+            description=(
+                f"Duration: {current}/{total}\n"
+                f"Volume: {state.volume*100}%"
+            ),
+            url=np.metadata.source_url,
+        ).set_footer(
+            text=np.metadata.artist or "Unkown Artist",
+        ).set_thumbnail(
+            np.metadata.thumbnail,
         )
         await ctx.respond(embed=embed)
